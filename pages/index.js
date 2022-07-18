0b5656e697bd314816e8dfabf520f39a9ec00b36
Brian_Branca <brianbranca@foxtrotco.com>
2022-07-18 17:24:25 -0500

    beginning to add context

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 // import Image from "next/image";
 import Footer from "../public/components/Footer";
-import HomeContainer from "../public/components/HomeContainer"
+import HomeContainer from "../public/components/HomeContainer";
 import Navigation from "../public/components/Navigation";
 // import ImageGrid from "../public/components/ImageGrid";
 import { AppContainer } from "../public/components/styled";
+import { ThemeContext } from "../public/theme-context";
 
 export default function Home() {
   return (
-    <AppContainer>
-      <Navigation />
-      <HomeContainer/>
-      <Footer />
-    </AppContainer>
+    <ThemeContext.Provider >
+      <AppContainer>
+        <Navigation />
+        <HomeContainer />
+        <Footer />
+      </AppContainer>
+    </ThemeContext.Provider>
   );
 }
